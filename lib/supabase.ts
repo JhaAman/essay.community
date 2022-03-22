b40794e6507b2648a9a897b7c3b60986e0e06017
Aman Jha <gunujha@gmail.com>
2022-03-21 21:14:33 -0700

    i hate managing emails. loops is good

---
+++ lib/supabase.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Found in .env.development and .env.production
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+export default createClient(supabaseUrl, supabaseAnonKey);
+export const getServiceSupabase = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.SUPABASE_SERVICE_KEY as string
+  );
+
+// TODO: do we need client options?
